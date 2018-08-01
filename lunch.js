2031396ce652de3b3c257e3abda625500f3225a4
sandradadda <dadda.sandra@yahoo.com>
2018-08-01 15:53:39 -0400

    first commit

---
+++ lunch.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    var sand1 = document.getElementById('mondayImage')
+    sand1.addEventListener('click', function (e) {
+        document.getElementById('mondayImage'),src = "sandwich.jpg"
+    }
+}
